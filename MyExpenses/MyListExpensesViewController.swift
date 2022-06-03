06fe51b2c4706c0f8aa09ca15f67aa0fa44564a6
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-06-03 21:47:24 +0300

    SetupNavigationBar

--- MyExpenses/MyListExpensesViewController.swift
+++ MyExpenses/MyListExpensesViewController.swift
@@ -11,8 +11,25 @@ class MyListExpensesViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .white
+        setupNavigationBar()
+        
+    }
+    
+    private func setupNavigationBar() {
         title = "Мои расходы"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewExpenses)
+        )
+        
+    }
+    
+    @objc private func addNewExpenses() {
+        
     }
 
 
