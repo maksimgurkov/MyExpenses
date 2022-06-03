e76f2d654afd8e7b641b4244e675104d31dc3845
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-06-03 21:39:58 +0300

    Start project

---
+++ MyExpenses/MyListExpensesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MyListExpensesViewController.swift
+//  MyExpenses
+//
+//  Created by Максим Гурков on 03.06.2022.
+//
+
+import UIKit
+
+class MyListExpensesViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Мои расходы"
+    }
+
+
+}
+
